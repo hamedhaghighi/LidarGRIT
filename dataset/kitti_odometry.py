changing fpd metric calculation and adding visualisation and removing bug in 1 nna

dataset/kitti_odometry.py
@@ -34,7 +34,7 @@ class  KITTIOdometry(torch.utils.data.Dataset):
         limited_view=False,
         finesize=None,
         norm_label=False,
-        do_augment=False):
+        do_gp=False):
         super().__init__()
         self.root = root if name == 'kitti_360' else osp.join(root, "sequences")
         if '/' in split:
@@ -65,7 +65,7 @@ class  KITTIOdometry(torch.utils.data.Dataset):
             transforms.Resize(self.shape, TF.InterpolationMode.NEAREST) 
             ]
         self.transform_aug_list = [transforms.RandomRotation(degrees=45, fill=1.0), transforms.RandomVerticalFlip(p=0.5), transforms.RandomHorizontalFlip(p=0.5),\
-                                             transforms.RandomAffine(degrees=30, translate=(0.1, 0.1), scale=(0.9, 1.1), fill=1.0)] if do_augment and split == 'train' else None
+                                             transforms.RandomAffine(degrees=30, translate=(0.1, 0.1), scale=(0.9, 1.1), fill=1.0)] if do_gp and split == 'train' else None
         if self.has_rgb:
             self.has_rgb = True
             calib = self.load_calib()

evaluate_from_samples.py
@@ -31,41 +31,40 @@ from util.visualizer import visualize_tensor
 os.environ['LD_PRELOAD'] = "/usr/lib/x86_64-linux-gnu/libstdc++.so.6" 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-EVAL_MAX_DEPTH = 63.0
-EVAL_MIN_DEPTH = 0.5
-DATASET_MAX_DEPTH = 80.0
-# EVAL_MAX_DEPTH = 80.0
-# EVAL_MIN_DEPTH = 1.45
-def depth_to_xyz(depth, lidar, tol=1e-8, cpu=False):
+
+def depth_to_xyz(depth, lidar, tol=1e-8, cpu=False, num_points=512):
     depth = tanh_to_sigmoid(depth).clamp_(0, 1)
     xyz = lidar.depth_to_xyz(depth, tol)
     xyz_out = xyz.flatten(2)
     xyz = xyz_out.transpose(1, 2)  # (B,N,3)
     if cpu:
-        xyz = downsample_point_clouds(xyz.cuda(), 512).cpu()
+        xyz = downsample_point_clouds(xyz.cuda(), num_points).cpu()
     else:
-        xyz = downsample_point_clouds(xyz, 512)
+        xyz = downsample_point_clouds(xyz, num_points)
     return xyz, xyz_out
 
 def to_np(tensor):
     return tensor.detach().cpu().numpy()
 
 class Features10k(torch.utils.data.Dataset):
-    def __init__(self, root, N, lidargen=False):
+    def __init__(self, root, min_depth, max_depth, lidargen=False, fpd=False):
         self.lidargen = lidargen
-        self.sample_path_list = sorted(Path(root).glob("*.pth"))[:N]
+        self.fpd = fpd
+        self.sample_path_list = sorted(Path(root).glob("*.pth"))
+        self.min_depth, self.max_depth = min_depth, max_depth
     def __getitem__(self, index):
         sample_path = self.sample_path_list[index]
         img = torch.load(sample_path, map_location="cpu")
         if self.lidargen:
             assert img.shape[0] == 2, img.shape
-        else:
-            assert img.shape[0] == 5, img.shape
 
         if self.lidargen:
             img[[0]] = torch.exp2(img[[0]]*6) - 1
         depth = img[[0]]
-        mask = torch.logical_and(depth > EVAL_MIN_DEPTH, depth < EVAL_MAX_DEPTH).float()
+        if self.fpd:
+            mask = torch.logical_and(depth > 0.5, depth < 63.0).float() # based on lidargen evaluation
+        else:
+            mask = torch.logical_and(depth > self.min_depth, depth < self.max_depth).float()
         img = img * mask
         return img.float(), mask.float()
 
@@ -83,77 +82,78 @@ def subsample(batch, n):
 
 def main(runner_cfg_path=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='', help='Path of the config file')
     parser.add_argument('--sample_dir', type=str, default='', help='Path of the sample dir')
     parser.add_argument('--data_dir', type=str, default='', help='Path of the sample dir')
     parser.add_argument('--ref_dataset_name', type=str, default='kitti_360', help='Path of the sample dir')
     parser.add_argument('--fast_test', action='store_true', help='fast test of experiment')
-    parser.add_argument('--cpu', action='store_true', help='fast test of experiment')
-    parser.add_argument('--lidargen', action='store_true', help='fast test of experiment')
+    parser.add_argument('--cpu', action='store_true', help='run on cpu')
+    parser.add_argument('--fpd', action='store_true', help='calculate fpd or other metrics (fpd requires different pre-processing)')
+    parser.add_argument('--debug', action='store_true', help='debugging through visualisation of samples')
+    parser.add_argument('--lidargen', action='store_true', help='set true if you want to evaluate lidargen')
     parser.add_argument('--gpu', type=int, default=0, help='GPU no')
-
+    parser.add_argument('--num_points', type=int, default=512, help='num of points for fps')
+    parser.add_argument('--num_samples', type=int, default=5000, help='num of samples for comparison')
     cl_args = parser.parse_args()
+    EVAL_MAX_DEPTH = 80.0
+    EVAL_MIN_DEPTH = 1.45
+    if cl_args.ref_dataset_name == 'kitti':
+        EVAL_MAX_DEPTH = 120.0
+        EVAL_MIN_DEPTH = 0.9
     if not cl_args.cpu:
         torch.cuda.set_device(f'cuda:{cl_args.gpu}')
     torch.manual_seed(0)
     np.random.seed(0)
     random.seed(0)
-  # create a visualizer that display/save images and plots
-    ## initilisation of the model for netF in cut
     device = torch.device(f'cuda:{cl_args.gpu}') if not cl_args.cpu else torch.device('cpu')
     data_dict = defaultdict(list)
     data_dict = torch.load(cl_args.data_dir, map_location=device)
-    N = 512 if cl_args.fast_test else  min(len(data_dict['real-2d']) * 8, 5000)
     print('data_dict loaded ...')
     ds_cfg = make_class_from_dict(yaml.safe_load(open(f'configs/dataset_cfg/{cl_args.ref_dataset_name}_cfg.yml', 'r')))
     ds_cfg.min_depth, ds_cfg.max_depth = EVAL_MIN_DEPTH, EVAL_MAX_DEPTH
     min_depth , max_depth = ds_cfg.min_depth, ds_cfg.max_depth
-    fpd_cls = FPD(None, cl_args.ref_dataset_name, None, device='cpu' if cl_args.cpu else 'cuda')
+    fpd_cls = FPD(None, cl_args.ref_dataset_name, None, max_sample=cl_args.num_samples, device='cuda')
     #### Train & Validation Loop
     # Train loop
     data_dict['synth-2d'] = [] 
     data_dict['synth-3d'] = []
-    if cl_args.ref_dataset_name == 'kitti_360':
+    if cl_args.ref_dataset_name == 'kitti_360'and not cl_args.fpd:
         data_dict['synth-bev'] = []
     fpd_points = []
-    # fid_samples = [] if fid_cls is not None else None
     h = 64 
     w = 1024 if cl_args.ref_dataset_name == 'kitti_360' else 256
-    lidar = LiDAR(
-    cfg=ds_cfg,
-    height=h,
-    width=w).to(device)
-    dataset = Features10k(cl_args.sample_dir, N, cl_args.lidargen)
-    gen_loader = DataLoader(dataset, batch_size=8, num_workers=4, shuffle=True)
+    lidar = LiDAR(cfg=ds_cfg, height=h, width=w).to(device)
+    dataset = Features10k(cl_args.sample_dir, min_depth, max_depth, cl_args.lidargen, cl_args.fpd)
+    gen_loader = DataLoader(dataset, batch_size=8, num_workers=4, shuffle=True, drop_last=False)
+    N = 8 if cl_args.fast_test else  cl_args.num_samples
     gen_loader_iter = iter(gen_loader)
     n_gen_batch = N//8 if cl_args.fast_test else  len(gen_loader) 
     gen_tq = tqdm.tqdm(total=n_gen_batch, desc='Iterating gen data', position=0, leave=True)
     for i in range(n_gen_batch):
         img, mask = next(gen_loader_iter)
         img = img.to(device)
+        mask = mask.to(device)
         synth_depth = (img[:, [0]] - min_depth) / (max_depth - min_depth)
         synth_depth = synth_depth * mask
         synth_depth = synth_depth * 2 - 1
         data_dict['synth-2d'].append(synth_depth)
-        xyz_ds , xyz_norm = depth_to_xyz(synth_depth, lidar, cpu=cl_args.cpu)
-        # import matplotlib.pyplot as plt
-        # plt.figure(0)
-        # plt.imshow(synth_depth[0,0].numpy(), cmap='jet')
-        # plt.figure(1)
-        # plt.imshow(data_dict['real-2d'][0][0,0].cpu().numpy(), cmap='jet')
-        # plt.figure(2)
-        # bev_img, pts_img = visualize_tensor(to_np(xyz_norm.transpose(1, 2)[0]), to_np((synth_depth[0] + 1)/2), 'point', cl_args.ref_dataset_name)
-        # plt.imshow(np.asarray(pts_img))
-        # plt.figure(3)
-        # bev_img, pts_img = visualize_tensor(to_np(((img[:, 1:4]).flatten(2).transpose(1, 2) / max_depth)[0]), to_np((synth_depth[0] + 1)/2), 'point', cl_args.ref_dataset_name)
-        # plt.imshow(np.asarray(pts_img))
-        # plt.show()
-        # exit(1)
-        # xyz = (img[:, 1:4]).flatten(2).transpose(1, 2)
-        # xyz_norm = xyz / max_depth
-        # xyz_ds = downsample_point_clouds(xyz_norm, 512)
+        xyz_ds , xyz_norm = depth_to_xyz(synth_depth, lidar, cpu=cl_args.cpu, num_points=cl_args.num_points)
+        if cl_args.debug:
+            import matplotlib.pyplot as plt
+            plt.figure(0)
+            plt.imshow(synth_depth[0,0].numpy(), cmap='jet')
+            plt.figure(1)
+            plt.imshow(data_dict['real-2d'][0][0,0].cpu().numpy(), cmap='jet')
+            plt.figure(2)
+            bev_img, pts_img = visualize_tensor(to_np(xyz_norm.transpose(1, 2)[0]), to_np((synth_depth[0] + 1)/2), 'point', cl_args.ref_dataset_name)
+            plt.imshow(np.asarray(pts_img))
+            plt.figure(3)
+            bev_img, pts_img = visualize_tensor(to_np(((img[:, 1:4]).flatten(2).transpose(1, 2) / max_depth)[0]),\
+             to_np((synth_depth[0] + 1)/2), 'point', cl_args.ref_dataset_name)
+            plt.imshow(np.asarray(pts_img))
+            plt.show()
+            exit(1)
         data_dict['synth-3d'].append(xyz_ds)
-        if cl_args.ref_dataset_name == 'kitti_360':
+        if cl_args.ref_dataset_name == 'kitti_360' and not cl_args.fpd:
             for pc in xyz_norm.transpose(1, 2):
                 hist = bev.point_cloud_to_histogram(pc * max_depth)
                 data_dict['synth-bev'].append(hist[None, ...].to(device))
@@ -163,16 +163,19 @@ def main(runner_cfg_path=None):
     for k ,v in data_dict.items():
         if isinstance(v, list):
             data_dict[k] = torch.cat(v, dim=0)[: N]
-    scores = {}
-    scores.update(compute_swd(subsample(data_dict["synth-2d"], 2048), subsample(data_dict["real-2d"], 2048)))
-    scores["jsd"] = compute_jsd(subsample(data_dict["synth-3d"], 2048) / 2.0, subsample(data_dict["real-3d"], 2048) / 2.0)
-    scores.update(compute_cov_mmd_1nna(subsample(data_dict["synth-3d"], 2048), subsample(data_dict["real-3d"], 2048), 512, ("cd",)))
-    if cl_args.ref_dataset_name == 'kitti_360':
-            scores["jsd-bev"] = bev.compute_jsd_2d(data_dict["real-bev"], data_dict["synth-bev"])
-            scores["mmd-bev"] = bev.compute_mmd_2d(data_dict["real-bev"], data_dict["synth-bev"])
-    if fpd_cls is not None:
-        scores['fpd'] = fpd_cls.fpd_score(torch.cat(fpd_points, dim=0))
-    print(scores)
+    n_subsample = 5000 if cl_args.ref_dataset_name == 'kitti' else 5000
+    if cl_args.fpd:
+        fpd_score = fpd_cls.fpd_score(torch.cat(fpd_points, dim=0))
+        print(f'fpd: {fpd_score}')
+    else:
+        scores = {}
+        scores.update(compute_swd(subsample(data_dict["synth-2d"], n_subsample), subsample(data_dict["real-2d"], n_subsample)))
+        scores["jsd"] = compute_jsd(subsample(data_dict["synth-3d"], n_subsample) / 2.0, subsample(data_dict["real-3d"], n_subsample) / 2.0)
+        scores.update(compute_cov_mmd_1nna(subsample(data_dict["synth-3d"], n_subsample), subsample(data_dict["real-3d"], n_subsample), 512, ("cd",)))
+        if cl_args.ref_dataset_name == 'kitti_360':
+                scores["jsd-bev"] = bev.compute_jsd_2d(data_dict["real-bev"], data_dict["synth-bev"])
+                scores["mmd-bev"] = bev.compute_mmd_2d(data_dict["real-bev"], data_dict["synth-bev"])
+        print(scores)
 
 
 if __name__ == '__main__':

train.py
@@ -24,6 +24,7 @@ from util.metrics.fpd import FPD
 from util.metrics import bev  
 import random
 import hashlib
+import math
 
 os.environ['LD_PRELOAD'] = "/usr/lib/x86_64-linux-gnu/libstdc++.so.6" 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -42,12 +43,15 @@ def hash_string(input_string):
     return hashed_string
 
 
-def depth_to_xyz(depth, lidar, tol=1e-8):
+def depth_to_xyz(depth, lidar, tol=1e-8, num_points=512, cpu=False):
     depth = tanh_to_sigmoid(depth).clamp_(0, 1)
     xyz = lidar.depth_to_xyz(depth, tol)
     xyz_out = xyz.flatten(2)
     xyz = xyz_out.transpose(1, 2)  # (B,N,3)
-    xyz = downsample_point_clouds(xyz, 512)
+    if cpu:
+        xyz = downsample_point_clouds(xyz.cuda(), num_points).cpu()
+    else:
+        xyz = downsample_point_clouds(xyz, num_points)
     return xyz, xyz_out
 
 def subsample(batch, n):
@@ -66,7 +70,7 @@ class M_parser():
         if data_dir != '':
             self.dataset.dataset_A.data_dir = data_dir
         if batch_size != '':
-            self.training.batch_size = batch_size
+            self.training.batch_size = int(batch_size)
         self.training.test = is_test
         self.model.isTrain = self.training.isTrain = not self.training.test
         self.training.epoch_decay = self.training.n_epochs//2
@@ -124,7 +128,7 @@ def check_exp_exists(opt, cfg_args):
         os.makedirs(exp_dir, exist_ok=True)
         shutil.copy(cfg_path, exp_dir)
     else:
-        assert os.path.exists(exp_dir)
+        assert os.path.exists(exp_dir), exp_dir
 
 def main(runner_cfg_path=None):
     parser = argparse.ArgumentParser()
@@ -137,8 +141,11 @@ def main(runner_cfg_path=None):
     parser.add_argument('--batch_size', type=str, default='', help='batch_size used in test exp')
     parser.add_argument('--test', action='store_true', help='test the model')
     parser.add_argument('--fast_test', action='store_true', help='fast test of experiment')
+    parser.add_argument('--debug', action='store_true', help='debug through visualisation')
     parser.add_argument('--ref_dataset_name', type=str, default='', help='reference dataset name for measuring unsupervised metrics')
     parser.add_argument('--n_fid', type=int, default=1000, help='num of samples for calculation of fid')
+    parser.add_argument('--num_points', type=int, default=512, help='num points for furthest point sampling')
+    parser.add_argument('--num_samples', type=int, default=5000, help='num samples for testing')
     parser.add_argument('--no_inv', action='store_true', help='use it to calc unsupervised metrics on input inv, in case modality_B does not contain inv')
     parser.add_argument('--on_input', action='store_true', help='unsupervised metrics will be calculated on dataset A')
     parser.add_argument('--gpu', type=int, default=0, help='GPU no')
@@ -199,29 +206,32 @@ def main(runner_cfg_path=None):
     model.setup(opt.training)
     fid_cls = FID(seg_model, test_dataset, cl_args.ref_dataset_name, lidar_A) \
         if (cl_args.ref_dataset_name!= '' and 'reflectance' in opt.model.modality_B) else None
-    fpd_cls = FPD(test_dataset, cl_args.ref_dataset_name, lidar_A) if cl_args.ref_dataset_name!= '' else None
+    fpd_cls = FPD(test_dataset, cl_args.ref_dataset_name, lidar_A, max_sample=cl_args.num_samples) if cl_args.ref_dataset_name!= '' else None
     n_test_batch = 2 if cl_args.fast_test else  len(test_dl)
     test_dl_iter = iter(test_dl)
     data_dict = defaultdict(list)
-    N = 2 * opt.training.batch_size if cl_args.fast_test else min(len(test_dataset), len(val_dataset), 5000)
+    N = 2 * opt.training.batch_size if cl_args.fast_test else min(len(test_dataset), len(val_dataset), cl_args.num_samples)
     H, W = opt.dataset.dataset_A.img_prop.height, opt.dataset.dataset_A.img_prop.width
-    data_dict_path = osp.join('stats', f'{cl_args.ref_dataset_name}_{H}*{W}_data_dict.pkl')
+    if cl_args.num_samples == 5000:
+        data_dict_path = osp.join('stats', f'{cl_args.ref_dataset_name}_n_{cl_args.num_points}_{H}*{W}_data_dict.pkl')
+    else:
+        data_dict_path = osp.join('stats', f'{cl_args.ref_dataset_name}_{cl_args.num_samples}_n_{cl_args.num_points}_{H}*{W}_data_dict.pkl')
     if not osp.exists(data_dict_path):
-        test_tq = tqdm.tqdm(total=N//opt.training.batch_size, desc='real_data', position=5)
-        for i in range(0, N, opt.training.batch_size):
+        test_tq = tqdm.tqdm(total=math.ceil(N / opt.training.batch_size), desc='real_data', position=5)
+        for i in range(math.ceil(N / opt.training.batch_size)):
             data = next(test_dl_iter)
             data = fetch_reals(data, lidar_ref, device, opt.model.norm_label)
-            data_dict['real-2d'].append(data['depth'])
-            xyz_ds, xyz = depth_to_xyz(data['depth'], lidar_ref)
-            data_dict['real-3d'].append(xyz_ds)
-            if not opt.training.isTrain or cl_args.ref_dataset_name == 'kitti_360':
+            data_dict['real-2d'].append(data['depth'].to(device if opt.training.isTrain else 'cpu'))
+            xyz_ds, xyz = depth_to_xyz(data['depth'], lidar_ref, num_points=cl_args.num_points)
+            data_dict['real-3d'].append(xyz_ds.to(device if opt.training.isTrain else 'cpu'))
+            if cl_args.ref_dataset_name == 'kitti_360':
                 for pc in xyz.transpose(1, 2):
                     hist = bev.point_cloud_to_histogram(pc * lidar.max_depth)
-                    data_dict['real-bev'].append(hist[None, ...].to(device))
+                    data_dict['real-bev'].append(hist[None, ...].to(device if opt.training.isTrain else 'cpu'))
             test_tq.update(1)
         torch.save(data_dict, data_dict_path)
     else:
-        data_dict = torch.load(data_dict_path, map_location=device)
+        data_dict = torch.load(data_dict_path, map_location=device if opt.training.isTrain else 'cpu')
         print('data_dict loaded ...')
     
     start_from_epoch = model.schedulers[0].last_epoch if opt.training.continue_train else 0 
@@ -240,14 +250,15 @@ def main(runner_cfg_path=None):
             train_tq = tqdm.tqdm(total=n_train_batch, desc='Iter', position=3)
             for i in range(n_train_batch):  # inner loop within one epoch
                 data = next(train_dl_iter)
-                # import matplotlib.pyplot as plt
-                # plt.figure(0)
-                # plt.imshow(np.clip(data['depth'][0,0].numpy()* 5, 0, 1))
-                # plt.figure(1)
-                # plt.imshow(np.clip(data['reflectance'][0,0].numpy(),0 ,1))
-                # plt.figure(2)
-                # plt.imshow(data['label'][0,0].numpy())
-                # plt.show()
+                if cl_args.debug:
+                    import matplotlib.pyplot as plt
+                    plt.figure(0)
+                    plt.imshow(np.clip(data['depth'][0,0].numpy()* 5, 0, 1))
+                    plt.figure(1)
+                    plt.imshow(np.clip(data['reflectance'][0,0].numpy(),0 ,1))
+                    plt.figure(2)
+                    plt.imshow(data['label'][0,0].numpy())
+                    plt.show()
                 iter_start_time = time.time()  # timer for computation per iteration
                 g_steps += 1
                 e_steps += 1
@@ -269,7 +280,7 @@ def main(runner_cfg_path=None):
                     model.save_networks('latest')
                 train_tq.update(1)
         val_dl_iter = iter(val_dl)
-        n_val_batch = 2 if cl_args.fast_test else (N//opt.training.batch_size if not opt.training.isTrain and is_transformer else len(val_dl))
+        n_val_batch = 2 if cl_args.fast_test else (math.ceil(N/opt.training.batch_size) if not opt.training.isTrain and is_transformer else len(val_dl))
 
         ##### validation
         val_losses = defaultdict(list)
@@ -315,14 +326,14 @@ def main(runner_cfg_path=None):
                     else:
                         synth_depth = fetched_data['depth'] * synth_mask
                 if len(data_dict['synth-2d']) * opt.training.batch_size < N:
-                    data_dict['synth-2d'].append(synth_depth)
-                    xyz_ds , xyz = depth_to_xyz(synth_depth, lidar)
-                    data_dict['synth-3d'].append(xyz_ds)
+                    data_dict['synth-2d'].append(synth_depth.to(device if opt.training.isTrain else 'cpu'))
+                    xyz_ds , xyz = depth_to_xyz(synth_depth, lidar, num_points=cl_args.num_points)
+                    data_dict['synth-3d'].append(xyz_ds.to(device if opt.training.isTrain else 'cpu'))
                     if not opt.training.isTrain and cl_args.ref_dataset_name == 'kitti_360':
                         for pc in xyz.transpose(1, 2):
                             hist = bev.point_cloud_to_histogram(pc * lidar.max_depth)
-                            data_dict['synth-bev'].append(hist[None, ...].to(device))
-                    fpd_points.append(xyz)
+                            data_dict['synth-bev'].append(hist[None, ...].to(device if opt.training.isTrain else 'cpu'))
+                    fpd_points.append(xyz.to(device if opt.training.isTrain else 'cpu'))
                 if fid_cls is not None and len(fid_samples) < cl_args.n_fid:
                     synth_depth = tanh_to_sigmoid(synth_depth)
                     synth_points = lidar.depth_to_xyz(tanh_to_sigmoid(synth_depth)) * lidar.max_depth
@@ -367,23 +378,25 @@ def main(runner_cfg_path=None):
         ##### calculating unsupervised metrics
 
         if not is_transformer or not opt.training.isTrain:
-            os.makedirs(osp(exp_dir, 'samples'), exist_ok=True)
-            index = 0
-            for batch in data_dict['synth-2d']:
-                for d in batch:
-                    d = tanh_to_sigmoid(d).clamp_(0, 1)
-                    mask = (d > 1e-8).float()
-                    unorm_d = d * (lidar.max_depth - lidar.min_depth) + lidar.min_depth
-                    unorm_d = unorm_d * mask
-                    torch.save(unorm_d.cpu(), osp.join(exp_dir, 'samples', f'{index}.pth'))
-                    index += 1
+            if is_transformer:
+                os.makedirs(osp.join(exp_dir, f'samples_{cl_args.num_samples}'), exist_ok=True)
+                index = 0
+                for batch in data_dict['synth-2d']:
+                    for d in batch:
+                        d = tanh_to_sigmoid(d).clamp_(0, 1)
+                        mask = (d > 1e-8).float()
+                        unorm_d = d * (lidar.max_depth - lidar.min_depth) + lidar.min_depth
+                        unorm_d = unorm_d * mask
+                        torch.save(unorm_d.cpu(), osp.join(exp_dir, f'samples_{cl_args.num_samples}', f'{index}.pth'))
+                        index += 1
             for k ,v in data_dict.items():
                 if isinstance(v, list):
                     data_dict[k] = torch.cat(v, dim=0)[: N]
             scores = {}
-            scores.update(compute_swd(subsample(data_dict["synth-2d"], 2048), subsample(data_dict["real-2d"], 2048)))
-            scores["jsd"] = compute_jsd(subsample(data_dict["synth-3d"], 2048) / 2.0, subsample(data_dict["real-3d"], 2048) / 2.0)
-            scores.update(compute_cov_mmd_1nna(subsample(data_dict["synth-3d"], 2048), subsample(data_dict["real-3d"], 2048), 512, ("cd",)))
+            n_subsample = 5000
+            scores.update(compute_swd(subsample(data_dict["synth-2d"], n_subsample), subsample(data_dict["real-2d"], n_subsample)))
+            scores["jsd"] = compute_jsd(subsample(data_dict["synth-3d"], n_subsample) / 2.0, subsample(data_dict["real-3d"], n_subsample) / 2.0)
+            scores.update(compute_cov_mmd_1nna(subsample(data_dict["synth-3d"], n_subsample), subsample(data_dict["real-3d"], n_subsample), 512, ("cd",)))
             if not opt.training.isTrain and cl_args.ref_dataset_name == 'kitti_360':
                  scores["jsd-bev"] = bev.compute_jsd_2d(data_dict["real-bev"], data_dict["synth-bev"])
                  scores["mmd-bev"] = bev.compute_mmd_2d(data_dict["real-bev"], data_dict["synth-bev"])

util/metrics/fpd.py
@@ -10,14 +10,14 @@ from util.metrics.pointnet import pretrained_pointnet
 from tqdm import trange, tqdm
 
 class FPD():
-  def __init__(self, train_dataset, dataset_name, lidar, max_sample=10000, batch_size=8, device='cuda'):
+  def __init__(self, train_dataset, dataset_name, lidar, max_sample=5000, batch_size=8, device='cuda'):
     self.path = './'
     self.batch_size = batch_size
     ds = train_dataset
     n_samples = min(max_sample, len(train_dataset)) if train_dataset is not None else max_sample
     stat_root = os.path.join('stats', 'fpd_stats')
     os.makedirs(stat_root, exist_ok=True)
-    stat_dir = os.path.join(stat_root, f'fpd_{dataset_name}.pkl')
+    stat_dir = os.path.join(stat_root, f'fpd_{dataset_name}.pkl') if n_samples == 5000 else os.path.join(stat_root, f'fpd_{dataset_name}_{10000}.pkl')
     # parameters
     self.lidar = lidar 
     # concatenate the encoder and the head
@@ -35,7 +35,15 @@ class FPD():
             data = ds[ind]
             if 'B' in data:
               data = data['B']
-            samples.append(data['points'].to(self.device))
+            depth = data['depth'].to(self.device)
+            if dataset_name == 'kitti_360':
+              mask_1 = data['mask'].to(self.device)
+              unorm_depth = depth * (self.lidar.max_depth - self.lidar.min_depth) + self.lidar.min_depth
+              mask_2 = torch.logical_and(unorm_depth > 0.5, unorm_depth < 63.0).float() # based on lidargen evaluation
+              depth *= mask_1 * mask_2
+            xyz = self.lidar.depth_to_xyz(depth[None, ...], 1e-8)[0]
+            xyz = xyz.flatten(2)
+            samples.append(xyz)
         samples = torch.stack(samples, dim=0).flatten(2)
         self.mu_train , self.sigma_train = self.compute_stats(samples)
         pickle.dump({'mu': self.mu_train, 'sigma':self.sigma_train}, open(stat_dir, 'wb'))
@@ -52,7 +60,10 @@ class FPD():
     features_list = []
     for i in trange(int(n_batch), desc='extracting features for fpd'):
       data = data_tensor[i * self.batch_size: (i + 1) * self.batch_size]
-      feature = self.pointnet(data)
+      if data.is_cuda:
+        feature = self.pointnet(data)
+      else:
+        feature = self.pointnet(data.cuda())
       features_list.append(feature.detach().cpu().numpy())
     
     return np.concatenate(features_list, axis=0)

util/visualizer.py
@@ -27,8 +27,7 @@ else:
 
 def visualize_tensor(pts, depth, tag, ds_name):
 
-    # depth_range = np.exp2(lidar_range*6)-1
-    color = plt.cm.turbo(np.clip(depth, 0, 1).flatten())
+    color = plt.cm.jet(np.clip(depth, 0, 1).flatten())
     # color = depth
     # mask out invalid points
     xyz = pts
@@ -48,20 +47,10 @@ def visualize_tensor(pts, depth, tag, ds_name):
     else:
         mtl.point_size = 4
     mtl.shader = "defaultLit"
-    # render.scene.set_background([255, 255, 255, 1.0])
-    # render.scene.set_background([0, 0, 0, 1.0])
     render.scene.add_geometry("point cloud", pcd, mtl)
     render.scene.set_lighting(render.scene.LightingProfile.NO_SHADOWS, (1, 1, 1))
-    # render.scene.scene.enable_sun_light(True)
-    # render.scene.camera.look_at([0, 0, 0], [0, 0, 0], [0, 0, 1])
     bev_img = render.render_to_image()
-    # render.setup_camera(60.0, [0, 0, 0], [-0.2, 0, 0.1], [0, 0, 1])
-    # render.setup_camera(60.0, [0, 0, 0], [-0.8, -0.1, 0.3], [0, 0, 1])
-    if ds_name == 'kitti' or ds_name == 'carla':
-        # render.setup_camera(60.0, [0, 0, 0], [-0.3, 0, 0.5], [0, 0, 1])
-        render.setup_camera(60.0, [0, 0, 0], [-0.3, 0, 0.2], [0, 0, 1])
-    else:
-        render.setup_camera(60.0, [0, 0, 0], [0.08, -0.1, 0.5], [0, 0, 1])
+    render.setup_camera(60.0, [0, 0, 0], [-0.1, -0.2, 0.25], [0, 0, 1])
     pts_img = render.render_to_image()
     return bev_img, pts_img
 